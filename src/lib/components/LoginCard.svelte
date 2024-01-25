8e1023a7f217580e46907270b6dbe6d5b8eb711d
manimejia <noreply@manimejia.me>
2024-01-25 15:33:17 -0600

    fix login form alignment

--- src/lib/components/LoginCard.svelte
+++ src/lib/components/LoginCard.svelte
@@ -2,9 +2,9 @@
   import locale from "$lib/locale/en.json";
 </script>
 
-<div class="card bg-neutral text-center p-5 justify-center">
+<div class="card bg-neutral text-center p-5 w-max" style="margin: 0 auto;">
     <p class="px-5 text-left">&#8681; {locale.component.LoginCard.messagepubkey}</p>
-    <form method="POST" class="flex justify-between p-2">
+    <form method="POST" class="flex justify-between p-2 w-max">
         <input name="userid" class="input input-bordered input-primary w-[75%] " type="text" placeholder="{locale.component.LoginCard.input}"/>
         <button type="submit" class="hidden"></button>
         <button  class="btn w-[20%] btn-primary btn-disabled"><small>{locale.component.LoginCard.button}</small></button>
