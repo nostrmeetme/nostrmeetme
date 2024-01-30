16cb54b433c62bf7cb42cfb9363a7a9fc7532466
manimejia <noreply@manimejia.me>
2024-01-30 16:55:06 -0600

    minor style tweak and version update

--- src/lib/components/LoginCard.svelte
+++ src/lib/components/LoginCard.svelte
@@ -2,7 +2,7 @@
   import locale from "$lib/locale/en.json";
 </script>
 
-<div class="card bg-neutral text-center py-3 w-full" style="margin: 0 auto;">
+<div class="card bg-neutral text-center py-3 max-w-640" style="margin: 0 auto;">
     <p class="px-5 text-left text-sm">&#8681; {locale.component.LoginCard.messagepubkey}</p>
     <form method="POST" class="flex justify-between p-2">
         <input name="userid" class="input input-bordered input-primary w-[75%] " type="text" placeholder="{locale.component.LoginCard.input}"/>
