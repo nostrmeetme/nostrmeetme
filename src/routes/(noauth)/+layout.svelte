d9b4e475c2175393629f2e76a4acbc4a44b78cb1
manimejia <noreply@manimejia.me>
2024-01-30 16:45:49 -0600

    remove sponsor banner from footer

--- src/routes/(noauth)/+layout.svelte
+++ src/routes/(noauth)/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
   import "../../app.css";
-  import SponsorFooter from "$lib/components/SponsorFooter.svelte";
+  import NavFooter from "$lib/components/NavFooter.svelte";
   import AppHeader from "$lib/components/AppHeader.svelte";
 </script>
 <AppHeader></AppHeader>
 <main class="px-2 pt-2 pb-[100px] flex flex-col items-center">
     <slot />
 </main>
-<SponsorFooter></SponsorFooter>
+<NavFooter></NavFooter>
