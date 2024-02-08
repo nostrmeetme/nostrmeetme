2e0f86322c7b1b4ab2ab9d7b416d6ae214ea4134
manimejia <noreply@manimejia.me>
2024-02-08 16:52:29 -0600

    more route group changes

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../../app.css";
+    import { user } from "$lib/stores/user";
     import NavFooter from "$lib/components/NavFooter.svelte";
     import AppHeader from "$lib/components/AppHeader.svelte";
   </script>
@@ -7,4 +8,6 @@
   <main class="px-2 pt-5 pb-[100px] flex flex-col items-center">
       <slot />
   </main>
+{#if $user}
 <NavFooter></NavFooter>
+{/if}
